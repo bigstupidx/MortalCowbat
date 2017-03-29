220800d1043e778349be0a05500e42bbd7da2801
Michal Louda <michal.louda@chargedmonkey.com>
2017-03-30 00:07:14 +0200

    Animations update

---
+++ Assets/Scripts/Vis/Shadow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Vis
+{
+	public class Shadow : MonoBehaviour
+	{
+
+		float vertDistToPivot;
+		Transform pivot;
+
+		void Awake()
+		{
+			pivot = transform.parent.parent.FindChild("Pivot");
+			vertDistToPivot = transform.position.y - pivot.position.y;
+		}
+
+		void LateUpdate()
+		{
+			var posY = pivot.position.y + vertDistToPivot;
+			transform.SetPositionY(posY);
+		}
+	}
+}
+
