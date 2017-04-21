ed403fefe3b961b40c7650b95017ad40f477abd3
Michal Louda <michal.louda@chargedmonkey.com>
2017-04-21 20:36:15 +0200

    PS4 controller charging fix

--- Assets/Scripts/PS4Controller.cs
+++ Assets/Scripts/PS4Controller.cs
@@ -13,13 +13,15 @@ public class PS4Controller : Controller
 	KeyCode heavyAttack;
 	KeyCode jump;
 
+	public int Index = 1;
+
 	protected override void Init()
 	{
-		Enabled = false;
-		jump = KeyCode.JoystickButton3; 		 // TRIANGLE
-		fastAtack = KeyCode.JoystickButton2; 	 // CIRCLE
-		heavyAttack = KeyCode.JoystickButton1; 	 // CROSS
-		specialAttack = KeyCode.JoystickButton0; //SQUARE
+		//Enabled = false;
+//		jump = KeyCode.JoystickButton3; 		 // TRIANGLE
+//		fastAtack = KeyCode.JoystickButton2; 	 // CIRCLE
+//		heavyAttack = KeyCode.JoystickButton1; 	 // CROSS
+//		specialAttack = KeyCode.JoystickButton0; //SQUARE
 	}
 
 	void Update()
@@ -27,37 +29,39 @@ public class PS4Controller : Controller
 		if (!Enabled)
 			return;
 
-		if (Input.GetKey(left)) {
-			character.MoveH(-1);
-		} else if (Input.GetKey(right)) {
-			character.MoveH(1);	
-		}
-		if (Input.GetKey(up)) {
-			character.MoveV(1);
-		} else if (Input.GetKey(down)) {
-			character.MoveV(-1);	
-		}
-		if (Input.GetKeyDown(jump)) {
+//		if (Input.GetKey(left)) {
+//			character.MoveH(-1);
+//		} else if (Input.GetKey(right)) {
+//			character.MoveH(1);	
+//		}
+//		if (Input.GetKey(up)) {
+//			character.MoveV(1);
+//		} else if (Input.GetKey(down)) {
+//			character.MoveV(-1);	
+//		}
+
+
+		if (Input.GetButtonDown("Triangle"+Index)) {
 			character.Jump();			
 		}
-		if (Input.GetKeyDown(fastAtack)) {
+		if (Input.GetButtonDown("Circle"+Index)) {
 			character.FastAttack();			
 		}
-		if (Input.GetKeyDown(heavyAttack)) {
+		if (Input.GetButtonDown("Square"+Index)) {
 			character.HeavyAttack();			
 		}
 
-		if (Input.GetKeyUp(heavyAttack)) {
+		if (Input.GetButtonUp("Square" + Index)) {
 			character.ChargedAttackReleased();			
 		}
 
-		if (Input.GetKeyDown(specialAttack)) {
+		if (Input.GetButtonDown("Cross"+Index)) {
 			character.AttackSpecial();			
 		}
 
 
 		var dir = GetAnalogueJoystickDirection();
-		if (dir.sqrMagnitude > 0.01f) {
+		if (dir.sqrMagnitude > 0.01f * 0.01f) {
 			character.AiMove(dir);
 		}
 	}
@@ -65,8 +69,11 @@ public class PS4Controller : Controller
 	Vector2 GetAnalogueJoystickDirection()
 	{
 		Vector2 dir = Vector2.zero;
+		//dir.x = Input.GetAxis("HorizontalJoy"+Index);
+		//dir.y = Input.GetAxis("VerticalJoy"+Index);
 		dir.x = Input.GetAxis("Horizontal");
 		dir.y = Input.GetAxis("Vertical");
+
 
 		return dir;
 	}
