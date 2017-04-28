c7f0d2c1d8d47b5b9657ecaa2e821ea148e4dc31
Michal Louda <michal.louda@chargedmonkey.com>
2017-04-28 07:54:05 +0200

    Pause player stops controller as well

--- Assets/Scripts/Components/Pause.cs
+++ Assets/Scripts/Components/Pause.cs
@@ -9,12 +9,20 @@ namespace Battle.Comp
 		public void Perform()
 		{
 			GetComp<Animating>().SetAnimatorSpeed(0.0f);
+			var controller = GetComponent<Controller>();
+			if (controller) {
+				controller.Enabled = false;
+			}
 			Paused = true;
 		}
 
 		public void Cancel()
 		{
 			GetComp<Animating>().SetAnimatorSpeed(1.0f);
+			var controller = GetComponent<Controller>();
+			if (controller) {
+				controller.Enabled = true;
+			}
 			Paused = false;
 		}
 	}
