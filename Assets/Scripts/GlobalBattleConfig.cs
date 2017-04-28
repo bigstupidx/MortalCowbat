346623f3a7447432ea76a0119b818a5ba769430a
Michal Louda <michal.louda@chargedmonkey.com>
2017-04-28 09:18:29 +0200

    Randomized Daze duration to Config file

--- Assets/Scripts/GlobalBattleConfig.cs
+++ Assets/Scripts/GlobalBattleConfig.cs
@@ -5,5 +5,6 @@ public static class GlobalBattleConfig
 	public static float FastAttackChangeMaxDelay = 0.5f;
 	public static bool RandomizeNPCSpawnTime = true;
 	public static bool RandomizeDazeDuration = true;
+	public static float DazeRandomDuration = 0.3f;
 }
 
