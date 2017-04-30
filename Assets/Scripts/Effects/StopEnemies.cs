496c661dd51de5d03c59d6736e9c940297f56a1b
Michal Louda <michal.louda@chargedmonkey.com>
2017-04-30 18:47:13 +0200

    Stop enemies effect

---
+++ Assets/Scripts/Effects/StopEnemies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Battle.Comp;
+
+public class StopEnemies : Effect
+{
+	
+	public override void Run(GameObject go)
+	{
+		base.Run(go);
+		StartCoroutine(PlayCoroutine(go));
+	}
+
+	IEnumerator PlayCoroutine(GameObject go)
+	{
+		var npcs = GameObject.FindGameObjectsWithTag("NPC");
+		Array.ForEach(npcs, x=>x.GetComponent<Character>().GetComp<Pause>().Perform());
+		yield break;
+	}
+
+	void OnDestroy()
+	{
+		var npcs = GameObject.FindGameObjectsWithTag("NPC");
+		Array.ForEach(npcs, x=>x.GetComponent<Character>().GetComp<Pause>().Cancel());
+	}
+}
+
