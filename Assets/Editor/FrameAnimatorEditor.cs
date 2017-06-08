c6d3f005d00294b55ca77143e0731595877073dc
Michal Louda <michal.louda@chargedmonkey.com>
2017-06-08 09:27:28 +0200

    VIS: FrameAnimator introduced - allow easy reskinning

---
+++ Assets/Editor/FrameAnimatorEditor.cs
@@ -0,0 +1,54 @@
+﻿using Vis;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+
+[CustomEditor(typeof(FrameAnimator))]
+public class FrameAnimatorEditor : Editor 
+{
+	const int ButtonWidth = 70;
+	const int SmallButtonWidth = 40;
+
+	bool showDefaultInspector;
+
+	public override void OnInspectorGUI()
+	{
+		//base.OnInspectorGUI();
+		var myTarget = (FrameAnimator)target;
+
+		showDefaultInspector = EditorGUILayout.Toggle("Debug", showDefaultInspector);
+		if (showDefaultInspector) {
+			base.OnInspectorGUI();
+		}
+
+
+		for (int i = 0; i < myTarget.Sprites.Count; ++i) {
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField(i.ToString(), GUILayout.Width(10));
+			myTarget.Sprites[i] = (Sprite)EditorGUILayout.ObjectField("", myTarget.Sprites[i], typeof(Sprite), false, GUILayout.Width(100));
+			EditorGUILayout.BeginVertical();
+			if(GUILayout.Button("Up", GUILayout.Width(ButtonWidth))) {
+				myTarget.MoveUp(i);
+				break;
+			}
+			if(GUILayout.Button("Down", GUILayout.Width(ButtonWidth))) {
+				myTarget.MoveDown(i);
+				break;
+			}
+			EditorGUILayout.EndVertical();
+
+			if(GUILayout.Button("-", GUILayout.Width(SmallButtonWidth))) {
+				myTarget.RemoveFrame(i);
+				break;
+			}
+
+			EditorGUILayout.EndHorizontal();
+		}
+
+		if(GUILayout.Button("+")) {
+			myTarget.Add();
+		}
+	}
+}
+
---
+++ Assets/Scripts/Vis/FrameAnimator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Vis
+{
+	[ExecuteInEditMode]
+	public class FrameAnimator : MonoBehaviour
+	{
+		public List<Sprite> Sprites { get { return sprites; }}
+
+		[SerializeField]
+		SpriteRenderer sprRenderer;
+
+		[SerializeField]
+		List<Sprite> sprites;
+	
+		public float SpriteIndex;
+
+
+		public int GetSpriteIndex()
+		{
+			return (int) SpriteIndex;
+		}
+
+		public void RemoveFrame(int index)
+		{
+			Sprites.RemoveAt(index);
+		}
+
+		public void MoveUp(int index)
+		{
+			if (index > 0) {
+				var sprite = Sprites[index];
+				Sprites[index] = Sprites[index - 1];
+				Sprites[index - 1] = sprite;
+			}
+		}
+
+		public void MoveDown(int index)
+		{
+			if (index < Sprites.Count - 1) {
+				var sprite = Sprites[index];
+				Sprites[index] = Sprites[index + 1];
+				Sprites[index + 1] = sprite;
+			}
+		}
+
+		public void Add()
+		{
+			Sprites.Add(null);
+		}
+
+		void Update()
+		{
+			if (sprRenderer != null && GetSpriteIndex() < sprites.Count) {
+				sprRenderer.sprite = sprites[GetSpriteIndex()];		
+			}
+		}
+	}
+}
+
